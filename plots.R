b40d0cdfa5fa53b0c6162c12a169c6e4342d7b44
ska24679 <ska24679@gmail.com>
2014-06-06 22:13:51 -0500

    more changes

--- plots.R
+++ plots.R
@@ -1,4 +1,7 @@
-household=read.csv("household_power_consumption.txt",sep=";",header=TRUE,na.strings="?")
+
+hugedata=read.csv("../household_power_consumption.txt",sep=";",header=TRUE,na.strings="?")
+household=sqldf("select * from hugedata where (Date is '01/02/2007' or Date is '02/02/2007')")
 #I don't know if this is, strictly speaking, neccesary
-household$Time=strptime(household$Time,format="%H:%M:%S")
 household$Date=as.Date(household$Date)
+household$Time=strptime(household$Time,format="%H:%M:%S")
+

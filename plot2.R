078556effba14ce799e48e2ee25b8fcb2c408d56
ska24679 <ska24679@gmail.com>
2014-06-07 00:43:04 -0500

    cleaner

--- plot2.R
+++ plot2.R
@@ -1,9 +1,10 @@
 library(sqldf)
 #Grep fixes everything
 grep="grep -E '^2/2/2007|^1/2/2007|^Date'"
+date_format="%H:%M:%S %d/%m/%Y"
 file="household_power_consumption.txt"
 household=read.csv.sql(file,sep=";",header=TRUE,filter=grep)
-household$Time=strptime(paste(household$Time,household$Date),format="%H:%M:%S %d/%m/%Y")
+household$Time=strptime(paste(household$Time,household$Date),format=date_format)
 with(household,plot(Time,Global_active_power,type="l",ylab="Global Active Power (kilowatts)",xlab=""))
 dev.copy(png,"plot2.png")
 dev.off()
--- plot3.R
+++ plot3.R
@@ -1,13 +1,15 @@
 library(sqldf)
 #Grep fixes everything
 grep="grep -E '^2/2/2007|^1/2/2007|^Date'"
+date_format="%H:%M:%S %d/%m/%Y"
 file="household_power_consumption.txt"
+legend_labels=c("Sub_metering_1","Sub_metering_2","Sub_metering_3")
 household=read.csv.sql(file,sep=";",header=TRUE,filter=grep)
-household$Time=strptime(paste(household$Time,household$Date),format="%H:%M:%S %d/%m/%Y")
+household$Time=strptime(paste(household$Time,household$Date),format=date_format)
 with(household,plot(Time,Sub_metering_1,type="l",ylab="Energy sub metering",xlab=""))
 with(household,lines(Time,Sub_metering_2,type="l",col="blue"))
 with(household,lines(Time,Sub_metering_3,type="l",col="red"))
-legend("topright",c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),col=c("black","blue","red"),lty=1)
+legend("topright",legend_labels,col=c("black","blue","red"),lty=1)
 dev.copy(png,"plot3.png")
 dev.off()
 

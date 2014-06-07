b3b6a9dc1b86124b9082c8138ac83bb700635001
ska24679 <ska24679@gmail.com>
2014-06-06 23:07:33 -0500

    close

--- plots.R
+++ plots.R
@@ -1,6 +1,12 @@
 library(sqldf)
-household=read.csv.sql("household_power_consumption.txt",sep=";",header=TRUE,filter="grep -E '^2/2/2007|^1/2/2007'")
+#Grep fixes everything
+grep="grep -E '^2/2/2007|^1/2/2007|^Date'"
+file="household_power_consumption.txt"
+household=read.csv.sql(file,sep=";",header=TRUE,filter=grep)
 #I don't know if this is, strictly speaking, neccesary
 household$Date=as.Date(household$Date)
 household$Time=strptime(household$Time,format="%H:%M:%S")
+hist(household$Global_active_power,col="red",main="Global Active Power",xlab="Global Active Power (kilowatts)")
+dev.copy(png,"plot1.png")
+dev.off()
 
